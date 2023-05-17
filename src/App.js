46e22dd9a1013b50f7b217032b6af6d62424fe12
Kdddru <125441996+Kdddru@users.noreply.github.com>
2023-05-17 22:11:18 +0900

    0517

--- src/App.js
+++ src/App.js
@@ -3,16 +3,7 @@ import './App.css';
 import { firestore } from './data/fireBase';
 
 function App() {
-  const [value2,setValue2] = useState();
-  const myName = firestore.collection('myName');
-  useEffect(()=>{
-    const myName = firestore.collection('myName');
-    myName.doc("hkbg0ek0JPsF3BiU1jpC").get().then((doc)=>{
-      const value = doc.data();
-      setValue2(value);
-    })
-  },[])
-  console.log(value2);
+  my
   return (
     <div className="App">
     </div>
